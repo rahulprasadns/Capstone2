1897870e82668f0dd1b55cd0fd6ada721685b09d
Rahul Prasad <nagumantrirahul@gmail.com>
2022-06-04 03:49:54 -0400

    new changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect} from "react";
+import io from "socket.io-client";
+let endPoint = "http://1ocalhost:5000";
+let socket = io.connect(`${endPoint}`);
+const App = () => {
+  // create state using hooks
+  const [messages, setMessages] = useState ( ["Hello And Welcome"])
+  const [message, setMessage] = useState("")
 
-function App() {
+  useEffect(() => {
+    getMessages()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [messages.length])
+
+  const getMessages = () => {
+    socket.on("message", msg => {
+      setMessages([...messages, msg])
+    })
+  }
+
+  const onChange = e => {
+    setMessage(e.target.value)
+  }
+
+  const onClick = () => {
+    if(message !== ""){
+      socket.emit("message", message)
+      setMessage("")
+    } else {
+      alert("Please enter a message")
+    }
+  }
+
   return (
-    <div className="App">
-      <h>Virtual Travel Agent</h>
+    <div>
+      <h1>Chat App</h1>
+    
+      {messages.length > 0 && messages.map((msg) => {(
+        <div> <p> {msg} </p> </div>
+      )}
+      )}
+
+      <input type="text" value={message} onChange={e => onChange(e)} />
+      <button onClick={() => onClick()}>Send</button>
+
     </div>
-  );
+  )
 }
 
 export default App;

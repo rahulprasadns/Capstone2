bac6ad9ce299ae8c5e7a779a2c700d62b4f9b100
Rahul Prasad <nagumantrirahul@gmail.com>
2022-06-04 02:56:01 -0400

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h>Virtual Travel Agent</h>
+    </div>
+  );
+}
+
+export default App;
